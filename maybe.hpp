2504881b49e6fd263de15b1b5b088f2df3b49f4e
Zebulun Arendsee <zbwrnz@gmail.com>
2026-03-16 23:04:05 -0400

    Fix C++ type resolution

--- maybe.hpp
+++ maybe.hpp
@@ -2,6 +2,7 @@
 #define __MORLOC__MAYBE_HPP__
 
 #include <optional>
+#include <stdexcept>
 
 // fromMaybe :: ?a -> a
 // Unwrap an optional value (safe under a null guard)
@@ -9,5 +10,12 @@ template <class A>
 A morloc_fromMaybe(std::optional<A> x) {
     return x.value();
 }
+
+// Overload for coerced non-optional values (a -> ?a coercion)
+template <class A>
+A morloc_fromMaybe(A x) {
+    return x;
+}
+
 
 #endif
